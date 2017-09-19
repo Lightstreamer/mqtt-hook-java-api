65af264a6f33421fbbcdd0e758edfcd20e715248
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2017-09-19 17:42:54 +0200

    Modify code layout.

--- src/main/java/cool/mqtt/hooks/MqttMessage.java
+++ src/main/java/cool/mqtt/hooks/MqttMessage.java
@@ -7,56 +7,56 @@ package cool.mqtt.hooks;
  */
 public interface MqttMessage {
 
-    /**
-     * Gets the name of the topic to which this message is being published.
-     * 
-     * @return the topic name
-     */
-    String getTopicName();
+  /**
+   * Gets the name of the topic to which this message is being published.
+   * 
+   * @return the topic name
+   */
+  String getTopicName();
 
-    /**
-     * Gets the <i>Application Message</i> carried by this message.
-     * <p>
-     * The MQTT Protocol Specifications defines the Application Message as the
-     * payload of the <code>PUBLISH</code> Control Packet.
-     * 
-     * @return the byte array containing the <i>Application Message</i> info
-     */
-    byte[] getApplicationMessage();
+  /**
+   * Gets the <i>Application Message</i> carried by this message.
+   * <p>
+   * The MQTT Protocol Specifications defines the Application Message as the
+   * payload of the <code>PUBLISH</code> Control Packet.
+   * 
+   * @return the byte array containing the <i>Application Message</i> info
+   */
+  byte[] getApplicationMessage();
 
-    /**
-     * Gets the Quality of Service level with which this message has been
-     * delivered.
-     * 
-     * @return the Quality of Service level
-     */
-    QoS getQos();
+  /**
+   * Gets the Quality of Service level with which this message has been
+   * delivered.
+   * 
+   * @return the Quality of Service level
+   */
+  QoS getQos();
 
-    /**
-     * Gets the <i>retained</i> flag of this message.
-     * <p>
-     * The retain flag specifies whether the MQTT broker must store this
-     * message, in order to be immediately delivered to future clients as they
-     * subscribe to matching topics.
-     * 
-     * @return {@code true} if the <i>retained</i> flag is set, {@code false}
-     *         otherwise
-     */
-    boolean isRetained();
+  /**
+   * Gets the <i>retained</i> flag of this message.
+   * <p>
+   * The retain flag specifies whether the MQTT broker must store this
+   * message, in order to be immediately delivered to future clients as they
+   * subscribe to matching topics.
+   * 
+   * @return {@code true} if the <i>retained</i> flag is set, {@code false}
+   *         otherwise
+   */
+  boolean isRetained();
 
-    /**
-     * Gets the <i>duplicate</i> flag of this message.
-     * <p>
-     * Only for messages with {@code QoS level > 0}, the duplicate flag
-     * indicates that this message might have been re-sent because not
-     * acknowledged in an earlier attempt.
-     * <p>
-     * In case of {@link QoS#AT_MOST_ONCE} level, this method always returns
-     * {@code false}.
-     * 
-     * @return {@code true} if the <i>duplicate</i> flag is set, {@code false}
-     *         otherwise
-     */
-    boolean isDuplicate();
+  /**
+   * Gets the <i>duplicate</i> flag of this message.
+   * <p>
+   * Only for messages with {@code QoS level > 0}, the duplicate flag
+   * indicates that this message might have been re-sent because not
+   * acknowledged in an earlier attempt.
+   * <p>
+   * In case of {@link QoS#AT_MOST_ONCE} level, this method always returns
+   * {@code false}.
+   * 
+   * @return {@code true} if the <i>duplicate</i> flag is set, {@code false}
+   *         otherwise
+   */
+  boolean isDuplicate();
 
 }
