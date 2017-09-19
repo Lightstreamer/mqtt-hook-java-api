86ada46907a49a9c2ed87646fec727c402a89910
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2017-09-19 17:43:37 +0200

    Modify code layout.

--- src/main/java/cool/mqtt/hooks/QoS.java
+++ src/main/java/cool/mqtt/hooks/QoS.java
@@ -8,37 +8,38 @@ package cool.mqtt.hooks;
  */
 public enum QoS {
 
-    /**
-     * The singleton instance for <i>At most once</i> level. This has the
-     * numeric value of {@code 0}.
-     */
-    AT_MOST_ONCE(0),
+  /**
+   * The singleton instance for <i>At most once</i> level. This has the
+   * numeric value of {@code 0}.
+   */
+  AT_MOST_ONCE(0),
 
-    /**
-     * The singleton instance for <i>At least once</i> level. This has the
-     * numeric value of {@code 1}.
-     */
-    AT_LEAST_ONCE(1),
+  /**
+   * The singleton instance for <i>At least once</i> level. This has the
+   * numeric value of {@code 1}.
+   */
+  AT_LEAST_ONCE(1),
 
-    /**
-     * The singleton instance for <i>Exactly once</i> level. This has the
-     * numeric value of {@code 2}.
-     */
-    EXACTLY_ONCE(2);
+  /**
+   * The singleton instance for <i>Exactly once</i> level. This has the
+   * numeric value of {@code 2}.
+   */
+  EXACTLY_ONCE(2);
 
-    private int value;
+  private int value;
 
-    QoS(int value) {
-        this.value = value;
-    }
+  QoS(int value) {
+    this.value = value;
+  }
 
-    /**
-     * Gets the Quality of Service level {@code int} value.
-     * 
-     * @return the Quality of Service level, from 0 ({@code AT_MOST_ONCE}) to 2
-     *         ({@code EXACTLY_ONCE} once)
-     */
-    public int getValue() {
-        return value;
-    }
+  /**
+   * Gets the Quality of Service level {@code int} value.
+   * 
+   * @return the Quality of Service level, from 0 ({@code AT_MOST_ONCE}) to 2
+   *         ({@code EXACTLY_ONCE} once)
+   */
+  public int getValue() {
+    return value;
+  }
+
 }
