abd23cacef1aa271fa6e03d871e289c82e4dd128
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2017-09-21 15:33:39 +0200

    Improve code formatting.

--- src/main/java/cool/mqtt/hooks/MqttSubscription.java
+++ src/main/java/cool/mqtt/hooks/MqttSubscription.java
@@ -1,23 +1,21 @@
 package cool.mqtt.hooks;
 
 /**
- * An abstraction of the <code>SUBSCRIBE</code> Control Packet, sent by the
- * client and passed to the Hook, before being encoded and finally submitted to
- * the target MQTT broker.
+ * An abstraction of the <code>SUBSCRIBE</code> Control Packet, sent by the client and passed to the
+ * Hook, before being encoded and finally submitted to the target MQTT broker.
  */
 public interface MqttSubscription {
 
   /**
-   * Gets the topic filter indicating one or more topics to which the client
-   * subscribe.
+   * Gets the topic filter indicating one or more topics to which the client subscribe.
    * 
    * @return the topic filter
    */
   String getTopicFilter();
 
   /**
-   * Gets the maximum Quality Of Service level which the target MQTT broker is
-   * allowed to send Application Messages.
+   * Gets the maximum Quality Of Service level which the target MQTT broker is allowed to send
+   * Application Messages.
    * 
    * @return the maximum Quality Of Service level
    */
