8cfcacd350af6be28212b70802cdcdfaec17930f
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2018-01-09 17:40:58 +0100

    Add test cases for handling new SecurityParams interfaces.

--- src/test/java/cool/mqtt/hooks/utils/MqttBrokerConfigBuilderTest.java
+++ src/test/java/cool/mqtt/hooks/utils/MqttBrokerConfigBuilderTest.java
@@ -8,6 +8,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import cool.mqtt.hooks.MqttBrokerConfig;
 import cool.mqtt.hooks.MqttMessage;
 import cool.mqtt.hooks.QoS;
+import cool.mqtt.hooks.SecurityParams;
+
+import java.nio.file.Paths;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -23,13 +26,13 @@ public class MqttBrokerConfigBuilderTest {
 
   @Before
   public void setup() {
-    builder = new MqttBrokerConfigBuilder("tcp://localhost:1883");
+    builder = new MqttBrokerConfigBuilder("mqtts://localhost:8883");
   }
 
   @Test
   public void shouldBuildConfigWithDefaultValues() {
     MqttBrokerConfig config = builder.build();
-    assertThat(config.getAddress(), is("tcp://localhost:1883"));
+    assertThat(config.getAddress(), is("mqtts://localhost:8883"));
     assertThat(config.getClientIdPrefix(), nullValue());
     assertThat(config.getConnectionTimeout(), nullValue());
     assertThat(config.getKeepAlive(), nullValue());
@@ -37,15 +40,27 @@ public class MqttBrokerConfigBuilderTest {
     assertThat(config.getUsername(), nullValue());
     assertThat(config.getPassword(), nullValue());
     assertThat(config.getWillMessage(), nullValue());
+    assertThat(config.getSecurityParams(), nullValue());
   }
 
   @Test
   public void shouldBuildConfigWithCustomValues() {
-    MqttBrokerConfig config = builder.username("username").password("password")
-        .clientIdPrefix("client_id_prefix").connectionTimeout(5).keepAlive(10)
-        .willMessage("topic", new byte[] {1, 2, 3}, QoS.AT_MOST_ONCE, true).build();
+    MqttBrokerConfig config = builder
+      .username("username")
+      .password("password")
+      .clientIdPrefix("client_id_prefix")
+      .connectionTimeout(5)
+      .keepAlive(10)
+      .willMessage("topic", new byte[] {1, 2, 3}, QoS.AT_MOST_ONCE, true)
+      .securityProtocol("TLSV1.1")
+      .truststorePath(Paths.get("truststore.jks"))
+      .truststorePassword("truststorepassword".toCharArray())
+      .keystorePath(Paths.get("keystore.jks"))
+      .keystorePassword("keystorepassword".toCharArray())
+      .privateKeyPassword("privatekeypassword".toCharArray())
+      .build();
 
-    assertThat(config.getAddress(), is("tcp://localhost:1883"));
+    assertThat(config.getAddress(), is("mqtts://localhost:8883"));
     assertThat(config.getClientIdPrefix(), is("client_id_prefix"));
     assertThat(config.getConnectionTimeout(), is(5));
     assertThat(config.getKeepAlive(), is(10));
@@ -57,6 +72,50 @@ public class MqttBrokerConfigBuilderTest {
     assertThat(willMessage.getTopicName(), is("topic"));
     assertThat(willMessage.getQos(), is(QoS.AT_MOST_ONCE));
     assertThat(willMessage.getApplicationMessage(), is(new byte[] {1, 2, 3}));
+
+    SecurityParams securityParams = config.getSecurityParams();
+    assertThat(securityParams, notNullValue());
+    assertThat(securityParams.getTruststorePath(), is(Paths.get("truststore.jks")));
+    assertThat(securityParams.getTruststorePassword(), is("truststorepassword".toCharArray()));
+    assertThat(securityParams.getKeystorePath(), is(Paths.get("keystore.jks")));
+    assertThat(securityParams.getKeystorePassword(), is("keystorepassword".toCharArray()));
+    assertThat(securityParams.getPrivateKeyPassword(), is("privatekeypassword".toCharArray()));
+  }
+
+  @Test
+  public void shouldReturnSecurityParamsWhenProvidingSecurityProtocol() {
+    MqttBrokerConfig config = builder.securityProtocol("TLSV1.2").build();
+    SecurityParams secParams = config.getSecurityParams();
+    assertThat(secParams, notNullValue());
+    assertThat(secParams.getSecurityProtocol(), is("TLSV1.2"));
+  }
+
+  @Test
+  public void shouldReturnSecurityParamsWhenProvidingTruststore() {
+    MqttBrokerConfig config = builder.truststorePath(Paths.get("truststore.jks")).build();
+    SecurityParams secParams = config.getSecurityParams();
+    assertThat(secParams, notNullValue());
+    assertThat(secParams.getTruststorePath(), is(Paths.get("truststore.jks")));
+  }
+
+  @Test
+  public void shouldReturnSecurityParamsWhenProvidingKeystore() {
+    MqttBrokerConfig config = builder.keystorePath(Paths.get("keystore.jks")).build();
+    SecurityParams secParams = config.getSecurityParams();
+    assertThat(secParams, notNullValue());
+    assertThat(secParams.getKeystorePath(), is(Paths.get("keystore.jks")));
+  }
+
+  @Test
+  public void shouldNotReturnSecurityParams() {
+    MqttBrokerConfig config = builder.securityProtocol("").build();
+    assertThat(config.getSecurityParams(), nullValue());
+
+    config = builder.keystorePassword("keystorepassword".toCharArray()).build();
+    assertThat(config.getSecurityParams(), nullValue());
+
+    config = builder.privateKeyPassword("privatekeypassword".toCharArray()).build();
+    assertThat(config.getSecurityParams(), nullValue());
   }
 
   @Test
