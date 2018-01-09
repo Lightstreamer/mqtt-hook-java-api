37347c8f955f07a2f15c1e9462ac11c32243c51f
Gianluca Finocchiaro <gianluca.finocchiaro@lightstreamer.com>
2018-01-09 17:34:17 +0100

    Add getSecurityParams methods and documentation comments.

--- src/main/java/cool/mqtt/hooks/MqttBrokerConfig.java
+++ src/main/java/cool/mqtt/hooks/MqttBrokerConfig.java
@@ -3,19 +3,20 @@ package cool.mqtt.hooks;
 /**
  * An encapsulation of the MQTT broker configuration corresponding to the connection alias provided
  * by a client on connection establishment with MQTT&#46;Cool.
- * 
+ *
  * <p>The Hook is expected to supply an instance of {@code MqttBrokerConfig} (through an invocation
  * of {@link MQTTCoolHook#resolveAlias(String)}) if no static entries have been provided in the
  * <tt>mqtt_master_connector_conf.xml</tt> file for a given connection alias.
- * 
+ *
  * <p>The configuration is made up of:
  * <ul>
- * <li>A mandatory address of the MQTT broker.</li>
+ * <li>Mandatory address of the MQTT broker.</li>
  * <li>Optional connection timeout and keep alive interval values.</li>
- * <li>An optional ClientId prefix.</li>
+ * <li>Optional ClientId prefix.</li>
  * <li>Optional {@code username} and {@code password}, if the MQTT broker requires an authenticated
- * access,</li>
- * <li>An optional {@code Will Message}.</li>
+ * access.</li>
+ * <li>Optional {@code Will Message}.</li>
+ * <li>Optional set of parameters for encrypted connections.</li>
  * </ul>
  * 
  * @see MQTTCoolHook#resolveAlias(String)
@@ -23,26 +24,35 @@ package cool.mqtt.hooks;
 public interface MqttBrokerConfig {
   /**
    * Gets the address of the MQTT broker to connect to.
-   * 
+   *
    * <p>The address must be specified in one of the following URI forms:
    * <ul>
    * <li>{@code tcp://<host>:<port>}</li>
    * <li>{@code mqtt://<host>:<port>}</li>
+   * <li>{@code mqtts://<host>:<port>}</li>
+   * <li>{@code ssl://<host>:<port>}</li>
    * </ul>
-   * otherwise MQTT&#46;Cool will invalidate it and the client will be notified with an a error.
+   * otherwise MQTT&#46;Cool will invalidate it and the client will be notified with an error.
+   *
+   * <p>Note that use of {@code mqtts} and {@code ssl} triggers encrypted communication with the
+   * MQTT broker, therefore further parameters (provided through an instance of
+   * {@link SecurityParams}) might be required in order to properly support the setting up of secure
+   * channels.
    *
    * @return the address of the MQTT broker
+   *
+   * @see #getSecurityParams()
    */
   String getAddress();
 
   /**
    * Gets the ClientId prefix to be used for shared connections.
-   * 
+   *
    * <p>If the clients want to share a single connection, MQTT&#46;Cool concatenates the ClientId
    * prefix with a randomly generated suffix in order to make a unique ClientId for opening the
    * physical connection to the MQTT broker. Uniqueness of the ClientId is mandatory as multiple
    * shared connections may exist for the same MQTT broker.
-   * 
+   *
    * <p>In case of blank return value (that is, an empty, whitespace or {@code null} string), the
    * default ClientId prefix {@code "CLIENT_ID_PREFIX_PLACEHOLDER"} will be used.
    *
@@ -53,7 +63,7 @@ public interface MqttBrokerConfig {
 
   /**
    * Gets the connection timeout expressed in seconds.
-   * 
+   *
    * <p>In case of a {@code null} return value, a default value of {@code 5} seconds will be used.
    *
    * @return the connection timeout expressed in seconds, or {@code null} if the default value is
@@ -63,7 +73,7 @@ public interface MqttBrokerConfig {
 
   /**
    * Gets the keep alive interval expressed in seconds.
-   * 
+   *
    * <p>In case of a {@code null} return value, a default value of {@code 30} seconds will be used.
    *
    * @return the keep alive interval expressed in seconds, or {@code null} if the default value is
@@ -73,15 +83,15 @@ public interface MqttBrokerConfig {
 
   /**
    * Gets the {@code username} for authenticating with the MQTT broker.
-   * 
+   *
    * <p>If the MQTT broker does not require authentication, this method should return a {@code null}
    * value. Empty or blank values will be provided <i>as is</i> in the <tt>CONNECT</tt> Control
    * Packet built to connect to the broker.
-   * 
+   *
    * <p>Note that the {@code username} which may be provided by the client will take precedence over
    * the one returned by this method.
    *
-   * @return the {@code username} or {@code null} if no credential is provided
+   * @return the {@code username}, or {@code null} if no credential is provided
    * @see MqttConnectOptions#getUsername()
    */
   String getUsername();
@@ -103,13 +113,34 @@ public interface MqttBrokerConfig {
 
   /**
    * Gets the {@code Will Message} to be stored by the MQTT broker.
-   * 
+   *
    * <p>Note that the {@code Will Message} which may be provided by the client will take precedence
    * over the one returned by this method.
    *
-   * @return the {@code Will Message} to be stored by the MQTT broker if any, otherwise {@code null}
+   * @return the {@code Will Message} to be stored by the MQTT broker if any, or {@code null}
    * @see MqttConnectOptions#getWillMessage()
    */
   MqttMessage getWillMessage();
+
+  /**
+   * Gets the {@code SecureParams} instance to be used for supporting the setting up of encrypted
+   * connections to the MQTT broker.
+   * 
+   * <p>Note that this method will be invoked only in the case the address returned by
+   * {@link #getAddress()} specifies a <i>secure</i> schema.
+   *
+   * <p>Furthermore, if {@code null} is returned, the MQTT&#46;Cool server will try to establish a
+   * secure channel using the following default settings:
+   *
+   * <ul>
+   * <li>TLSv1.2 as security protocol</li>
+   * <li>Default truststore and keystore, determined according to the <a href=
+   * "https://docs.oracle.com/javase/7/docs/technotes/guides/security/jsse/JSSERefGuide.html">JSSE
+   * Reference Guide</a>.</li>
+   * </ul>
+   *
+   * @return a {@code SecureParams} instance, or {@code null}
+   */
+  SecurityParams getSecurityParams();
 
 }
